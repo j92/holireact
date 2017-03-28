b116b210819734685505c5b50976c6a457d67905
Joost van Driel <joostvdriel@gmail.com>
2017-03-28 19:41:02 +0200

    first commit

---
+++ src/components/Clock.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class Clock extends Component {
+
+    constructor() {
+        super();
+        this.state = {date: new Date()}
+    }
+
+    componentDidMount() {
+        setInterval(() => this.tick())
+    }
+
+    tick() {
+        this.setState({date: new Date()})
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello world</h1>
+                <h2>It is {this.state.date.toLocaleString()}</h2>
+            </div>
+        );
+    }
+}
+
+export default Clock;
